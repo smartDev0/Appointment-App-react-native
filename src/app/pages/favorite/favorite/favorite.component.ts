620ab625c3db33029fed9c7922d042f90e655d58
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-07-03 00:04:40 +0800

    add fovorite page done

--- src/app/pages/favorite/favorite/favorite.component.ts
+++ src/app/pages/favorite/favorite/favorite.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-favorite',
-  templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.scss'],
+  selector: "app-favorite",
+  templateUrl: "./favorite.component.html",
+  styleUrls: ["./favorite.component.scss"],
 })
 export class FavoriteComponent implements OnInit {
+  BannerOption = {
+    initialSlide: 0,
+    slidesPerView: 4.5,
+    loop: true,
+    centeredSlides: false,
+    autoplay: false,
+    speed: 500,
+    spaceBetween: 4,
+  };
+  cardOption = {
+    initialSlide: 0,
+    slidesPerView: 1.2,
+    loop: true,
+    autoplay: false,
+    speed: 500,
+    centeredSlides: true,
+    spaceBetween: 4,
+  };
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
