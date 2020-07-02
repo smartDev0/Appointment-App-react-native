d59bf7b9f789509188e1d34eef7ccc7be915508c
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-07-03 02:42:57 +0800

    add appointment and rate page done

--- src/app/pages/appointment/appointment/appointment.component.ts
+++ src/app/pages/appointment/appointment/appointment.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-appointment',
-  templateUrl: './appointment.component.html',
-  styleUrls: ['./appointment.component.scss'],
+  selector: "app-appointment",
+  templateUrl: "./appointment.component.html",
+  styleUrls: ["./appointment.component.scss"],
 })
 export class AppointmentComponent implements OnInit {
-
-  constructor() { }
+  segment = 0;
+  refresherEvent: any;
+  constructor(private router: Router) {}
 
   ngOnInit() {}
-
+  segmentChanged(event) {}
+  changeSlide(e) {
+    this.segment = e;
+    if (this.refresherEvent) {
+      this.refresherEvent.target.complete();
+    }
+  }
+  doRefresh(event) {
+    // this.refresherEvent = event;
+    // switch (this.segment) {
+    //   case 0:
+    //     break;
+    //   case 1:
+    //     break;
+    // }
+  }
+  goReview(id) {
+    this.router.navigate(["/rate/", id]);
+  }
 }
