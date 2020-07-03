0332f391a2b8926555574d066b66706d66752917
skyCoder0416 <53361119+skyCoder@users.noreply.github.com>
2020-07-03 08:48:03 +0800

    add home page some part

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  BannerOption = {
+    initialSlide: 0,
+    slidesPerView: 4.5,
+    loop: true,
+    centeredSlides: false,
+    autoplay: false,
+    speed: 500,
+    spaceBetween: 4,
+  };
+  constructor() {}
 
   ngOnInit() {}
-
 }
